208d6a163ec603cac5b5e7dc3d863c67fcb6bea8
Murilo0356 <murilorolim2005@gmail.com>
2026-09-23 02:29:37 -0300

    adcionar clienter no gateway

--- microservicos/gateway/server.js
+++ microservicos/gateway/server.js
@@ -11,6 +11,8 @@ const PRODUTOS_URL =
 const PEDIDOS_URL =
     process.env.PEDIDOS_URL || "http://localhost:3002";
 
+const CLIENTES_URL =
+    process.env.CLIENTES_URL || "http://localhost:3003";
 
 app.use("/produtos", async (req, res) => {
     try {
@@ -55,7 +57,27 @@ app.use("/pedidos", async (req, res) => {
         });
     }
 });
+app.use("/clientes", async (req, res) => {
+    try {
+        const resposta = await axios({
+            method: req.method,
+            url: `${CLIENTES_URL}${req.originalUrl}`,
+            data: req.body,
+            params: req.query,
+            timeout: 5000
+        });
 
+        res.status(resposta.status).json(resposta.data);
+    } catch (erro) {
+        if (erro.response) {
+            return res.status(erro.response.status).json(erro.response.data);
+        }
+
+        return res.status(503).json({
+            erro: "Serviço de Clientes indisponível"
+        });
+    }
+});
 
 app.listen(3000, () => {
     console.log("Gateway rodando na porta 3000");
